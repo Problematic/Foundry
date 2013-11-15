eb6f0f8a771cb34a2e4d2240d776507f896c6f0e
Derek Stobbe <djstobbe@gmail.com>
2013-11-15 15:37:58 -0700

    Support CommonJS and AMD loaders

--- build/outro.js
+++ build/outro.js
@@ -1,3 +1,14 @@
-    var global = window || global;
-    global.Foundry = Foundry;
+    if (typeof define === 'function' && define.amd === 'object' && define.amd) {
+        define(function () {
+            return Foundry;
+        });
+    } else if (typeof exports !== 'undefined') {
+        if (typeof module !== 'undefined' && module.exports) {
+            exports = module.exports = Foundry;
+        }
+        exports = Foundry;
+    } else {
+        var global = window || global;
+        global.Foundry = Foundry;
+    }
 }());
